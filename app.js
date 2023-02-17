6ae218637cf11b2ee8f42087deb035216da6937b
Kieran <49917350+KRawl8@users.noreply.github.com>
2023-02-17 15:54:40 +0000

    Add some JS interactions

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
-let myLibrary = [theHobbit, aGameOfThrones];
+let myLibrary = [];
+// theHobbit, aGameOfThrones
+
+// const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, 1);
+
+// const aGameOfThrones = new Book(
+//   "A Game of Thrones",
+//   "George.R.R.Martin",
+//   694,
+//   1
+// );
+
+const addBookButton = document.querySelector(".add-book"); // Identifies "Add Book +" button
+addBookButton.addEventListener("click", () => {
+  createNewBook();
+});
+
+// Opens up the pop-up form to enter in new book details
+const popupForm = document.querySelector(".popup-form");
+function createNewBook() {
+  popupForm.style.display = "block";
+}
+
+// When the add book button is pressed all of the forms inputs gets passed to the object constructor
+const enterBookButton = document.querySelector(".enter-book");
+enterBookButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  let title = document.querySelector(".title");
+  let newTitle = title.textContent;
+  let author = document.querySelector(".author");
+  let newAuthor = author.textContent;
+  let pages = document.querySelector(".pages");
+  let newPages = pages.textContent;
+  let newRead = document.querySelector(".read");
+  newBook = new Book(newTitle, newAuthor, newPages, newRead);
+  myLibrary.push(newBook);
+  popupForm.style.display = "none";
+});
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -10,16 +47,4 @@ function Book(title, author, pages, read) {
   };
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, 1);
-// console.log(theHobbit.info());
-
-const aGameOfThrones = new Book(
-  "A Game of Thrones",
-  "George.R.R.Martin",
-  694,
-  1
-);
-
-function addBookToLibrary() {
-  // do stuff here
-}
+// function addBookToLibrary() {}
