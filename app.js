974242cde0cfdb6ca235a5c98fd302ef46ca2190
Kieran <49917350+KRawl8@users.noreply.github.com>
2023-02-22 15:49:22 +0000

    New books now show up in Library

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 let myLibrary = [];
 // theHobbit, aGameOfThrones
 
-// const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, 1);
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
+myLibrary.push(theHobbit);
 
-// const aGameOfThrones = new Book(
-//   "A Game of Thrones",
-//   "George.R.R.Martin",
-//   694,
-//   1
-// );
+const aGameOfThrones = new Book(
+  "A Game of Thrones",
+  "George.R.R Martin",
+  694,
+  true
+);
+myLibrary.push(aGameOfThrones);
 
 const addBookButton = document.querySelector(".add-book"); // Identifies "Add Book +" button
 addBookButton.addEventListener("click", () => {
@@ -26,15 +28,17 @@ const enterBookButton = document.querySelector(".enter-book");
 enterBookButton.addEventListener("click", (event) => {
   event.preventDefault();
   let title = document.querySelector(".title");
-  let newTitle = title.textContent;
+  let newTitle = title.value;
   let author = document.querySelector(".author");
-  let newAuthor = author.textContent;
+  let newAuthor = author.value;
   let pages = document.querySelector(".pages");
-  let newPages = pages.textContent;
-  let newRead = document.querySelector(".read");
-  newBook = new Book(newTitle, newAuthor, newPages, newRead);
+  let newPages = pages.value;
+  let read = document.querySelector(".read");
+  let newRead = read.checked;
+  let newBook = new Book(newTitle, newAuthor, newPages, newRead);
   myLibrary.push(newBook);
   popupForm.style.display = "none";
+  libraryUpdate();
 });
 
 function Book(title, author, pages, read) {
@@ -42,9 +46,43 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    return `${title} by ${author}, ${pages} pages, has been read ${read} times`;
+  this.info = () => {
+    return `${title} by ${author}, ${pages} pages.`;
   };
 }
 
-// function addBookToLibrary() {}
+const libraryUpdate = () => {
+  let bookSection = document.querySelector(".book-section");
+  while (bookSection.firstChild) {
+    bookSection.removeChild(bookSection.lastChild);
+  }
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    let card = document.createElement("div");
+    card.setAttribute("class", "book");
+    let cardContent = document.createElement("div");
+    cardContent.setAttribute("class", "book-content");
+    let title = document.createElement("p");
+    title.textContent = myLibrary[i].title;
+    let author = document.createElement("p");
+    author.textContent = myLibrary[i].author;
+    let pages = document.createElement("p");
+    pages.textContent = myLibrary[i].pages;
+    let read = document.createElement("button");
+    if (myLibrary[i].read === false) {
+      read.textContent = "Not Read";
+      read.style.backgroundColor = "red";
+    } else if (myLibrary[i].read === true) {
+      read.textContent = "Read";
+      read.style.backgroundColor = "green";
+    }
+
+    card.appendChild(cardContent);
+    cardContent.appendChild(title);
+    cardContent.appendChild(author);
+    cardContent.appendChild(pages);
+    cardContent.appendChild(read);
+
+    bookSection.appendChild(card);
+  }
+};
